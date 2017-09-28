5f3c769a9ece0ff1c2d310510e4b6ed9b64280dc
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-09-28 14:39:15 -0400

    I made progress on the ears!

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -21,18 +21,42 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let c = Canvas(width: 300, height: 500)
+
+// Make the background blue
+c.fillColor = Color.blue
+c.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 300, height: 500)
 
 // Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
+c.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
 
 // Show where the origin is
-canvas.drawAxes()
+c.drawAxes()
 
 // Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+c.borderColor = Color.red
+c.defaultBorderWidth = 10
+c.fillColor = Color.green
+c.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+c.drawRectangle(centreX: 50, centreY: 50, width: 20, height: 20)
 
 
 // This code is necessary to see the result in the Assistant Editor at right
-PlaygroundPage.current.liveView = canvas.imageView
+PlaygroundPage.current.liveView = c.imageView
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
